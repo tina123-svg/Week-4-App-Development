dcacc1a26b5538871f02e0823f0122574e6f94fc
tina123-svg <tinasitaula835@gmail.com>
2025-11-18 12:06:59 +0545

    Task 3

---
+++ Task 3/ParameterDemo.cs
@@ -0,0 +1,21 @@
+﻿public class ParameterDemo
+{
+    public void Increase(ref int number)
+    {
+        number += 10;
+    }
+    public void DisplayMessage(out string fullname)
+    {
+        fullname = "Tina Sitaula";
+    }
+
+    public int SumAll(params int[] numbers)
+    {
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+}
--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ParameterDemo demo = new ParameterDemo();
+
+        int value = 5;
+        Console.WriteLine("Original Number: " + value);
+
+        demo.Increase(ref value);
+        Console.WriteLine("Increased Number: " + value);
+
+        demo.DisplayMessage(out string fullname);
+        Console.WriteLine("Returned Full Name: " + fullname);
+
+        int sum = demo.SumAll(1, 2, 3, 4, 5);
+        Console.WriteLine("Sum of the elements of array: " + sum);
+
+    }
+}

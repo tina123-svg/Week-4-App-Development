bce07711f4b400cb224598a8868e49b91e0d03ec
tina123-svg <tinasitaula835@gmail.com>
2025-11-18 12:20:44 +0545

    Task 4

---
+++ Task 4/Player.cs
@@ -0,0 +1,21 @@
+﻿public class Player
+{
+    public string playerName;
+    public int level;
+    public int health;
+
+    public Player()
+    {
+        playerName = "Player 1";
+        level = 1;
+        health = 100;
+    }
+
+    public Player(string name, int level, int health)
+    {
+        this.playerName = name;
+        this.level = level;
+        this.health = health;
+    }
+
+}
---
+++ Task 4/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Using default constructor
+        Player player1 = new Player();
+
+        // Using parameterized constructor
+        Player player2 = new Player("Tina ", 5, 80);
+
+        // Values of player one.
+        Console.WriteLine("Player One:");
+        Console.WriteLine("Name: " + player1.playerName);
+        Console.WriteLine("Level: " + player1.level);
+        Console.WriteLine("Health: " + player1.health);
+
+        // Values of player two.
+        Console.WriteLine("\nPlayer Two:");
+        Console.WriteLine("Name: " + player2.playerName);
+        Console.WriteLine("Level: " + player2.level);
+        Console.WriteLine("Health: " + player2.health);
+    }
+}

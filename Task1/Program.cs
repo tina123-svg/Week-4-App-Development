2663c667234936d3fd7d92adab90b5c98d97515e
tina123-svg <tinasitaula835@gmail.com>
2025-11-18 11:38:11 +0545

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Student student1 = new Student();
+        student1.Name = "Tina Sitaula";
+        student1.RollNumber = 101;
+        student1.Section = "L3C8";
+
+        Student student2 = new Student();
+        student2.Name = "Asmita Katwal";
+        student2.RollNumber = 102;
+        student2.Section = "L3C8";
+
+        //? Printing the instakce fields
+
+        //? Printing the details of student1
+        Console.WriteLine("Student 1 Details:");
+        Console.WriteLine("Name: " + student1.Name);
+        Console.WriteLine("Roll Number: " + student1.RollNumber);
+        Console.WriteLine("Section: " + student1.Section);
+
+        //? Printing the details of student2
+        Console.WriteLine("\nStudent 2 Details:");
+        Console.WriteLine("Name: " + student2.Name);
+        Console.WriteLine("Roll Number: " + student2.RollNumber);
+        Console.WriteLine("Section: " + student2.Section);
+
+        //? Printing the static fields
+        Console.WriteLine("College Name: " + Student.CollegeName);
+    }
+}
---
+++ Task1/Student.cs
@@ -0,0 +1,11 @@
+﻿public class Student
+{
+    //? Declaring the instance fields 
+    public string Name;
+    public int RollNumber;
+    public string Section;
+
+    //? Declaring the static field
+    public static String CollegeName = "Itahari Interntional COllege";
+
+}

4ccd9d1b7026d798d35c64878734cbf1e75749b3
tina123-svg <tinasitaula835@gmail.com>
2025-11-19 12:27:57 +0545

    Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the marks: ");
+        string marksValue = Console.ReadLine();
+
+        Console.Write("Enter the total marks: ");
+        string totalValue = Console.ReadLine();
+
+        //? By using try parse
+        int marks, total;
+
+        if (!int.TryParse(marksValue, out marks))
+        {
+            Console.WriteLine("Invalid input for marks.");
+            return;
+        }
+        if (!int.TryParse(totalValue, out total))
+        {
+            Console.WriteLine("Invalid input for total marks.");
+            return;
+        }
+
+        double percentage = marks/total * 100;
+
+        Console.WriteLine($"The percentage: {percentage}%");
+    }
+}
